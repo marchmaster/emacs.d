18ac63a3009e85a331d3715c98dcf192edfc834e
phoenix <marchmaster@sina.com>
2017-01-09 22:01:48 +0800

    Emacs main init file

---
+++ init.el
@@ -0,0 +1,62 @@
+;; Emacs Core Configuration wrote by matrix
+;; ----------------------------------------
+
+;; Package Installation Part 
+
+(require 'package)
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+; Package Lists
+(defvar myPackages
+  '(better-defaults
+    elpy
+    evil
+    flycheck
+    material-theme))
+
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+
+;; BASIC CUSTOMIZATION
+;; --------------------------------------
+
+(setq inhibit-startup-message t) ;; hide the startup message
+(load-theme 'material t) ;; load material theme
+(global-linum-mode t) ;; enable line numbers
+(setq mac-option-modifier 'meta) ;; set option to meta
+
+;; Elpy Related Configuration
+(elpy-enable) ;;
+;(elpy-use-ipython)
+
+;; Evil Package enable
+(require 'evil)
+(evil-mode 1)
+
+;;use flycheck not flymake
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook' 'flycheck-mode))
+
+;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (magit material-theme better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
